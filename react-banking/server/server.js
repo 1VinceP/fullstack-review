f1834559275d2bfd0cb5453d6c3f11fe512c52d0
Vincent Palmer <vpalmergraphics@gmail.com>
2017-09-07 12:10:40 -0600

    auth0 working

--- react-banking/server/server.js
+++ react-banking/server/server.js
@@ -19,13 +19,59 @@ app.use( session({
 app.use( passport.initialize() );
 app.use( passport.session() );
 
+// Connect to database via massive
 massive(process.env.CONNECTIONSTRING).then( db => {
-    app.set( 'db', db );
-} )
+    app.set( 'db', db )
+} );
+
+//////////////////////////////////////////////////////////////////////////////////// AUTH0 ////////////////////////////////////////////////////////////////////////
+// THIS IS THE STRATEGY
+passport.use( new Auth0Strategy( {
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENT_ID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    callbackURL: process.env.AUTH_CALLBACK
+},
+    function( accessToken, refreshToken, extraParams, profile, done ) {
+        const db = app.get('db')
+        // If user exists, get their data. If not, create a new user
+        db.find_user( profile.id ).then( user => {
+            if( user[0] )
+                return done( null, user )
+            else {
+                db.create_user( [profile.displayName, profile.emails[0].value, profile.picture, profile.id] )
+                  .then( user => {
+                    return done( null, user[0] )
+                })
+            };
+        } );
+    }
+) );
+
+// This is invoked once to set things up
+passport.serializeUser( function( user, done ) {
+    done( null, user )
+} );
+// User comes from session - this is invoked for every endpoint
+passport.deserializeUser( function( user, done ) {
+    app.get('db').find_session_user( user[0].id ).then( user => {
+        return done( null, user[0] )
+    })
+} );
+
+// Directs user to auth0.com to enter credentials
+app.get( '/auth', passport.authenticate('auth0') );
+
+// Redirects user based on input
+app.get( '/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000/#/private',
+    failureRedirect: 'http://localhost:3000/#/'
+}) );
 
 
 
 let port = 3005;
+const portChalk = chalk.cyan.underline.bold;
 app.listen( port, () => {
-    console.log( chalk`{magenta Eavesdropping on port} {cyan.underline.bold ${port}}` )
+    console.log( portChalk(`Eavesdropping on port ${port}`) )
 });
